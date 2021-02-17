da4a1c3493c56eb611e5ee2cea694b1075d269a8
Jimmy Madon <hello@jimmymadon.com>
2021-02-17 11:25:24 +0000

    Day 2 Input-output

---
+++ 02_simple_statements/input_output.py
@@ -0,0 +1,22 @@
+print("Hello World!")
+print(10)
+print(True)
+
+welcomeMessage = "Welcome Jimmy!"
+print(welcomeMessage)
+
+# Addition (Concatenation or joining) of strings
+userName = "Jimmy"
+print(userName)
+userName = "Camille"
+print(userName)
+welcomeMessage2 = "Welcome " + userName + "!"
+print(welcomeMessage2)
+
+# Difference between int and string
+print(5 + 10)
+print("5 + 10")
+
+userName = input("Enter your username: ")
+welcomeMessage3 = "Welcome " + userName + "!"
+print(welcomeMessage3)
