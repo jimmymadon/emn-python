5137a6dd4f3807dfea721deba60ff6e7689c7328
Jimmy Madon <hello@jimmymadon.com>
2021-03-02 11:24:13 +0000

    All while loops exercises

--- 04_collections_loops/loops_while_2c.py
+++ 04_collections_loops/loops_while_2c.py
@@ -1,10 +1,18 @@
 # Given a list of words. find a list of words that are greater than 4 characters
-words = []
 
+# Create a while loop like in loops_2c.py
+words = []
+counter = 0
+while counter < 1000:
+    word = input("Please enter a word (or Q to exit): ")
+    if word == "Q":
+        break
+    words.append(word)
+    counter += 1
 
 result_list = []
 counter = 0
-while counter < 6:
+while counter < len(words):
     if len(words[counter]) > 4:
         result_list.append(words[counter])
     counter += 1
