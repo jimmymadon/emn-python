75464aaee4007e4cbe1c28704070b85d9cf1f819
Jimmy Madon <hello@jimmymadon.com>
2021-03-02 11:37:00 +0000

    Exercise for tomorrow

--- 04_collections_loops/improved_calculator_loops.py
+++ 04_collections_loops/improved_calculator_loops.py
@@ -1,23 +1,24 @@
-number1 = float(input("Enter the first number: "))
-print("Your first number is:", number1, "and the type is", type(number1))
-
-number2 = float(input("Enter the second number: "))
-print("Your second number is:", number2, "and the type is", type(number1))
-
 print("""
 Menu:
 add: Addition
 subtract: Subtraction
 multiply: Multiplication
 divide: Division
+quit: Quit the program
 """)
 chosen_operation = input("Enter a operation from the above menu: ")
 
-valid_options = ["add", "subtract", "multiply", "divide"]
+number1 = float(input("Enter the first number: "))
+print("Your first number is:", number1, "and the type is", type(number1))
+
+number2 = float(input("Enter the second number: "))
+print("Your second number is:", number2, "and the type is", type(number1))
+
+valid_options = ["add", "subtract", "multiply", "divide", "quit"]
 
 if chosen_operation not in valid_options:
     print("Invalid Option. Please retry!")
-    exit()
+    exit()  # continue (when using a loop)
 if chosen_operation == "add":
     addition_result = number1 + number2
     print("Addition Result: " + str(addition_result))
