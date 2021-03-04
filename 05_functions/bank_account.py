fb675a88a8198fbb002cdafe56fb31f834b0cd35
Jimmy Madon <hello@jimmymadon.com>
2021-03-04 09:48:41 +0000

    Bank account solution

--- 05_functions/bank_account.py
+++ 05_functions/bank_account.py
@@ -1,15 +1,38 @@
-balance = float(input("Please enter starting balance"))
+# define a function to deposit a given amount to the balance
+def deposit(balance):
+    deposited_amount = float(input("Enter amount to be deposited: "))
+    updated_balance = balance + deposited_amount
+    print("Amount deposited: ", deposited_amount)
+    print("Updated_balance: ", updated_balance)
+    return updated_balance
 
-# Define a function to deposit a given amount to the balance
 
-# Define a function to withdraw a given amount from the balance
+# define a function to withdraw a given amount from the balance
+def withdraw(balance):
+    withdrawn_amount = float(input("Enter amount to be withdrawn: "))
+    updated_balance = balance - withdrawn_amount
+    print("Amount withdrawn: ", withdrawn_amount)
+    print("Updated_balance: ", updated_balance)
+    return updated_balance
 
-# Call the function to deposit 20 pounds
 
-# Call the function to withdraw 10 pounds
+# define a function that takes the balance and it returns a string:
+# if balance > 10, return "You are rich!"
+# if balance < 10, return "Work harder!"
+def comment_on_balance(balance):
+    if balance > 10:
+        return "You are rich!"
+    else:
+        return "Work harder!"
 
-# Define a function that takes the balance, and it returns a string:
-# If balance > 10, return "You are rich!"
-# If balance <= 10, return "Work harder!"
 
-# Call the above function passing it the current balance
+current_balance = float(input("Please enter starting balance: "))
+
+# call the function to deposit x pounds
+current_balance = deposit(current_balance)
+
+# call the function to withdraw x pounds
+current_balance = withdraw(current_balance)
+
+# call the above function (give it a name) passing it the current balance
+print(comment_on_balance(current_balance))
